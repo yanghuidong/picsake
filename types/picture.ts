7a56370c72ae0bff9e7608a229730f61b0b665af
yanghuidong <huidong@treslumen.org>
2025-09-16 21:10:20 +0800

    ^ image link detection logic: replace regex with plain string processing, more control, more maintainable

--- types/picture.ts
+++ types/picture.ts
@@ -19,15 +19,33 @@ type CSSDimensions = {
 
 type PicturesByPath = { [key: string]: Picture[] };
 
+const IMAGE_EXT_LIST = [
+	'avif',
+	'gif',
+	'jpeg',
+	'jpg',
+	'png',
+	'svg',
+	'webp',
+];
+
 function isImageLink(link: string) {
-	const regex = /\.(avif|gif|jpe?g|png|svg|webp)$/i;
-	return regex.test(link);
+	// const regex = /\.(avif|gif|jpe?g|png|svg|webp)$/i;
+	// return regex.test(link);
+	const ext = extFromLink(link);
+	if (!ext) return false;
+	return IMAGE_EXT_LIST.contains(ext);
 }
 
 function imageFormatFromLink(link: string): string | null {
-	// if (!isImageLink(link)) return null;
-	const ext = link.split('.').pop()?.split(/[#?]/)[0];
-	if (!ext) return null;
+	const ext = extFromLink(link);
 	if (ext === 'jpg') return 'jpeg';
 	return ext;
+}
+
+function extFromLink(link: string): string | null {
+	if (!link.contains('.')) return null;
+	const ext = link.split('.').pop()?.split(/[#?]/)[0];
+	if (!ext) return null;
+	return ext.toLowerCase();
 }
